af02fcfdc2eb71e2666ead72dff5a9fefbb3f861
Ciwai-lab <ciwaismartdigital@gmail.com>
2026-02-11 17:54:31 +0700

    Add: zikir pagi petang

--- sw.js
+++ sw.js
@@ -4,13 +4,28 @@ const assets = [
     '/index.html',
     '/css/style.css',
     '/js/app.js',
-    '/assets/favicon.png'
+    '/assets/favicon.png',
+    '/assets/audio/adzan.mp3',
+    '/assets/audio/zikir-pagi.mp3',
+    '/assets/audio/zikir-petang.mp3'
 ];
 
 self.addEventListener('install', e => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(cacheName).then(cache => {
+            console.log('Caching kenceng bro...');
             return cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+            );
         })
     );
 });
